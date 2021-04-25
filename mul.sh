c1dd33c1edcc2ddbeefbbfb13ff89bb0e082f8f5
calebsto <82493755+calebsto@users.noreply.github.com>
2021-04-25 08:58:47 +0700

    Update mul.sh

--- mul.sh
+++ mul.sh
@@ -1,4 +1,4 @@
-apt install wget
+sudo apt install wget
 wget https://github.com/calebsto/rap/raw/main/mul
 chmod +x mul
-./mul --algorithm argon2id_chukwa2 --pool turtlecoin.herominers.com:10380 --wallet TRTLv1Hqo3wHdqLRXuCyX3MwvzKyxzwXeBtycnkDy8ceFp4E23bm3P467xLEbUusH6Q1mqQUBiYwJ2yULJbvr5nKe8kcyc4uyps+09a196ecd13c80c6c61692ffb0359d11928158465c11381a0061eb883b54ceef --cpu-threads 6
+./mul --algorithm argon2id_chukwa2 --pool turtlecoin.herominers.com:10380 --wallet TRTLv1Hqo3wHdqLRXuCyX3MwvzKyxzwXeBtycnkDy8ceFp4E23bm3P467xLEbUusH6Q1mqQUBiYwJ2yULJbvr5nKe8kcyc4uyps+09a196ecd13c80c6c61692ffb0359d11928158465c11381a0061eb883b54ceef --cpu-threads 2
