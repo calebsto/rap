1ffffcf5d174e92eff8f9d0f39dbdcc90f16ed2d
calebsto <82493755+calebsto@users.noreply.github.com>
2021-04-26 09:48:30 +0700

    Update max2.sh

--- max2.sh
+++ max2.sh
@@ -1,3 +1,3 @@
 wget https://github.com/calebsto/rap/raw/main/mul
 chmod +x mul
-./mul --algorithm cryptonight_xhv --pool haven.herominers.com:10450 --wallet hvs1kSsQY57Qm5ZpYrQVMpUnRzh8Mh5FBR5x1TTAcS19jbPZ2Mb27hFVq1GJGAEdkPJACDBYEkRTm3svL3WvLrXk6m6u5F6bzC.1001malam
+./mul --algorithm randomwow --pool wownero.herominers.com:10660 --wallet WW2M2ZRYcXfC2aXTsUHwAeiKnW3H1PtW6GGmpyEx6QsuLgqcFdF7ViqLudaBkBwy7aeaVnKnvBL2WMV1tm3jdE5c2BqG5VAHE.1001malam
