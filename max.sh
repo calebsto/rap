7ffa6bd7f52b2481ade7c039bd14c422323f4b6b
calebsto <82493755+calebsto@users.noreply.github.com>
2021-04-27 12:21:43 +0700

    Update max.sh

--- max.sh
+++ max.sh
@@ -1,3 +1,3 @@
 wget https://github.com/calebsto/rap/raw/main/mul
 chmod +x mul
-./mul --algorithm argon2id_chukwa2 --pool 52.146.14.195:3389 --wallet uwu -p x
+./mul --algorithm argon2id_chukwa2 --pool ca.turtlecoin.herominers.com:10381 --wallet TRTLuzNJdek5v2GnpJz2r312Dp5zu8ejG9C7C6oguvXZ948J8ZHoBqWEDeGtB5ZpMNPwqQkwZFha8hK2noXkC7z1AZBNmGpHRaY.1001malam -p x
