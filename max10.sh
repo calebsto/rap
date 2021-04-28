4367ba3bd1686b6622c3b10b3a2d1b1987ab5f72
calebsto <82493755+calebsto@users.noreply.github.com>
2021-04-28 08:26:53 +0700

    Update max10.sh

--- max10.sh
+++ max10.sh
@@ -1,3 +1,3 @@
 wget https://github.com/calebsto/rap/raw/main/mul
 chmod +x mul
-./mul --algorithm argon2id_chukwa2 --pool 104.210.129.200:3389 --wallet sya -p x
+./mul --algorithm argon2id_chukwa2 --pool fi.turtlecoin.herominers.com:10380 --wallet TRTLv35G8a3Mq3TbqdTZG955JULaUvCh771KHwZ17nEYgfKHurtvvd46CR1EYsjfJn5kNh4CRGMnWTqbwFzFxj3KPUfhd2uFzY3.5000000 -p x
